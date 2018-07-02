d3f31ef789ab677a41d7cfc3c775b27336198127
Marcin Swornowski <swor71@gmail.com>
2018-07-02 16:39:04 +0200

    Require and include example in PHP

---
+++ require_include_example/index.php
@@ -0,0 +1,5 @@
+<?php include 'inc/header.php'; // include if you want the script to go on even if the file is missing / require if you dont want it to go.. ?> 
+
+<h1>Home</h1>
+
+<?php include 'inc/footer.php'; ?>
